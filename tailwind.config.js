0b5851c1fe1b84214d2983c43a2a9a1d326782f6
wendyhamel <whamel@it4web.nl>
2024-09-19 18:00:13 +0200

    updated project

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["index.html"],
+	theme: {
+		fontFamily: {
+			'sans': ['Manrope', 'sans-serif'],
+		},
+		fontSize: {
+			'base': ['13px', '20px'],
+			'lg': ['16px', '24px'],
+			'xl': ['20px', '28px'],
+		},
+		colors: {
+			'blue-400': 'hsl(217, 19%, 35%)',
+			'blue-300': 'hsl(214, 17%, 51%)',
+			'blue-200': 'hsl(212, 23%, 69%)',
+			'blue-100': 'hsl(210, 46%, 95%)',
+			black: 'hsl(0, 0%, 0%)',
+			white: 'hsl(100,100%,100%)',
+		}
+	}
+}
